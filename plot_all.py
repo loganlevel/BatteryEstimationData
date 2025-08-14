353690a6b80a93006221ad45f03cff6ef137fa5e
Logan Hartford <logan.hartford@level.co>
2025-08-14 11:24:55 -0700

    plot all

--- plot_all.py
+++ plot_all.py
@@ -6,7 +6,7 @@ import numpy as np  # NEW
 from matplotlib.lines import Line2D
 
 # CONFIGURATION
-CSV_DIR = "csvs/temp-20"
+CSV_DIR = "csvs/temp-minus20"
 CSV_TAG = os.path.basename(os.path.normpath(CSV_DIR))
 PLOT_OUTPUT = f"plots/compare_all_{CSV_TAG}.png"
 
