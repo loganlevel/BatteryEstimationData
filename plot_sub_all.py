57641af8febe6c5434fa771cb5c999f7c2c3e92b
Logan Hartford <logan.hartford@level.co>
2025-08-25 10:23:56 -0700

    all-data-cross-temp

--- plot_sub_all.py
+++ plot_sub_all.py
@@ -6,7 +6,7 @@ import numpy as np
 from matplotlib.lines import Line2D
 
 # CONFIGURATION
-CSV_DIR = "csvs/all-data"
+CSV_DIR = "csvs/all-data-cross-temp"
 CSV_TAG = os.path.basename(os.path.normpath(CSV_DIR))
 PLOT_OUTPUT = f"plots/compare_all_{CSV_DIR.replace("/","-")}.png"
 COMPUTE_LOW_BATT_BY_ALL_FAULTS=False
